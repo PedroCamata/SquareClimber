446e051d6b251be9a74d51429e7ec9c0187cc666
Pedro Camata Andreon <pedrocamata@gmail.com>
2018-12-30 17:42:34 +0000

    First commit

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Platform : MonoBehaviour
+{
+
+    const float LADDER_MARGIN = 0.06f;
+    const float LADDER_TIME_CHANGE = 0.15f;
+    private float ladderTimer = 0;
+
+    public bool ladderBehaviour = false;
+
+    public bool checkPointBehaviour = false;
+    public bool lastCheckPoint = false;
+
+    public bool moveBehavior = false;
+    private Vector3 startPosition;
+    public Vector3 endPosition;
+    public float movementTime = 1f;
+    private bool isGoingToEndPosition = false;
+    private float movementTimer;
+    private Vector3 movement;
+
+    public int hitsToBreak = -1; // -1 Don't break
+
+    private Transform playerTransform;
+
+    private float ladderTopCorner = 0;
+    private BoxCollider2D boxCollider;
+
+    //static Color PLATFORM_SOLID_COLOR = Color.white;
+    static Color PLATFORM_LADDER_COLOR = Color.cyan;
+    static Color PLATFORM_CHECKPOINT_NORMAL_COLOR = Color.blue;
+    static Color PLATFORM_CHECKPOINT_SAVED_COLOR = Color.green;
+
+
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        ladderTopCorner = gameObject.transform.position.y + boxCollider.bounds.extents.y;
+
+        playerTransform = GameObject.Find(GameController.PLAYER_OBJ_NAME).transform;
+
+        if (checkPointBehaviour)
+        {
+            GetComponent<SpriteRenderer>().color = PLATFORM_CHECKPOINT_NORMAL_COLOR;
+            ladderBehaviour = true;
+        }
+        else if (ladderBehaviour)
+        {
+            GetComponent<SpriteRenderer>().color = PLATFORM_LADDER_COLOR;
+        }
+
+        if (moveBehavior)
+        {
+            startPosition = transform.localPosition;
+        }
+        movementTimer = movementTime;
+
+    }
+
+    void FixedUpdate()
+    {
+        ladderTransformation();
+        platformMovement();
+        checkPoint();
+    }
+
+    private void checkPoint()
+    {
+        if (checkPointBehaviour)
+        {
+            if (playerTransform.position.y - Player.playerBoundsY + LADDER_MARGIN > ladderTopCorner - LADDER_MARGIN)
+            {
+                GetComponent<SpriteRenderer>().color = PLATFORM_CHECKPOINT_SAVED_COLOR;
+                //transform.localScale += Vector3.right * GameController.SCREEN_WORLD_SPACE_SIZE_X * Time.deltaTime;
+                if (transform.localScale.x >= GameController.SCREEN_WORLD_SPACE_SIZE_X)
+                {
+                    if (lastCheckPoint)
+                    {
+                        // Restart scene
+                        Scene loadedLevel = SceneManager.GetActiveScene();
+                        SceneManager.LoadScene(loadedLevel.buildIndex);
+                        return;
+                    } else
+                    {
+
+                    checkPointBehaviour = false;
+                    ladderBehaviour = false;
+                    moveBehavior = false;
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void platformMovement()
+    {
+        if (moveBehavior)
+        {
+            movementTimer += Time.deltaTime;
+            if (movementTimer >= movementTime)
+            {
+                isGoingToEndPosition = !isGoingToEndPosition;
+
+                Vector3 goingTo = startPosition;
+                if (isGoingToEndPosition)
+                {
+                    goingTo = endPosition;
+                }
+                movement = ((goingTo - transform.localPosition) / movementTime); // Distance / Time
+                movementTimer = 0;
+            }
+
+            transform.Translate(movement * Time.deltaTime);
+
+        }
+    }
+
+    private void ladderTransformation()
+    {
+        if (ladderBehaviour)
+        {
+            if (moveBehavior)
+            {
+                ladderTopCorner = gameObject.transform.position.y + boxCollider.bounds.extents.y;
+            }
+
+            if (playerTransform.position.y - Player.playerBoundsY + LADDER_MARGIN > ladderTopCorner - LADDER_MARGIN)
+            {
+                // Make ladder collidable
+                boxCollider.enabled = true;
+            }
+            // When player move from over to under the ladder
+            // Start a timer to make the transition from collidable to not collidable right
+            else if (ladderTimer > LADDER_TIME_CHANGE)
+            {
+                // Make ladder not collidable
+                boxCollider.enabled = false;
+                ladderTimer = 0;
+            }
+            else
+            {
+                ladderTimer += Time.deltaTime;
+            }
+        }
+    }
+
+    //void OnCollisionEnter2D (Collider2D col) {
+    //    if(hitsToBreak > 0)
+    //    {
+    //        hitsToBreak--;
+    //        if (hitsToBreak == 0)
+    //        {
+    //            Destroy(this);
+    //        }
+    //    }
+    //}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    const float MOVEMENT_X_GROUND = 3;
+    const float MOVEMENT_X_AIR = 2.5F;
+    const float MOVEMENT_Y = 225;
+    
+
+    const float MARGIN_TO_COLLIDE = 0.05f;
+
+    // Jump Variables
+    const float JUMP_RELOAD = 0.15f;
+    private float timeReloadJump = 0;
+
+    const float JUMP_TIME_LIMIT_PRESS = 0.3f;
+    public bool pressedJump = false;
+    public float timeJumpPressed = 0;
+
+    public LayerMask groundLayer;
+
+    public bool canMove = true;
+    public bool isGrounded = false;
+
+
+    // Components
+    Rigidbody2D rb;
+    BoxCollider2D bc;
+
+    // Player bound for Ladder platform
+    public static float playerBoundsY;
+
+    private enum wallCollidingDirection { None, Left, Right, Booth };
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+        playerBoundsY = bc.bounds.extents.y;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Control();
+    }
+
+    public void Control()
+    {
+        Vector2 velocity = rb.velocity;
+
+        float moveX = MOVEMENT_X_AIR;
+
+        if (pressedJump && Input.GetAxis("Vertical") > 0 && timeJumpPressed < JUMP_TIME_LIMIT_PRESS)
+        {
+            timeJumpPressed += Time.deltaTime;
+            velocity.y = MOVEMENT_Y * Time.deltaTime;
+        }
+        else
+        {
+            pressedJump = false;
+        }
+
+        if (isGround())
+        {
+            pressedJump = false;
+            timeReloadJump -= Time.deltaTime;
+            moveX = MOVEMENT_X_GROUND;
+            velocity.x = 0;
+            if (Input.GetAxis("Vertical") > 0 && timeReloadJump <= 0)
+            {
+                // Jump
+                velocity.y = MOVEMENT_Y * Time.deltaTime;
+                timeReloadJump = JUMP_RELOAD;
+                timeJumpPressed = 0;
+                isGrounded = false;
+                pressedJump = true;
+
+            }
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                // DownTheLadder
+
+            }
+        }
+
+        wallCollidingDirection wallHitting = isHittingWall();
+        if (canMove && wallHitting != wallCollidingDirection.Booth)
+        {
+            if (Input.GetAxis("Horizontal") > 0 && wallHitting != wallCollidingDirection.Right)
+            {
+                // Right
+                velocity.x = moveX;
+            }
+            else if (Input.GetAxis("Horizontal") < 0 && wallHitting != wallCollidingDirection.Left)
+            {
+                // Left
+                velocity.x = -moveX;
+            }
+        }
+
+        rb.velocity = velocity;
+
+
+    }
+
+    #region Collisions
+
+    private bool isGround()
+    {
+        Vector2 position = transform.position;
+        position.x += bc.bounds.extents.x;
+        position.y -= bc.bounds.extents.y;
+
+        Vector2 position2 = transform.position;
+        position2.x -= bc.bounds.extents.x;
+        position2.y = position.y;
+
+        if (isCollidingInDirection(position, Vector3.down) || isCollidingInDirection(position2, Vector3.down))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private wallCollidingDirection isHittingWall()
+    {
+        wallCollidingDirection collidingDirection = wallCollidingDirection.None;
+
+        Vector2 positionR = transform.position;
+        positionR.x += bc.bounds.extents.x;
+        positionR.y += bc.bounds.extents.y;
+
+        Vector2 positionR2 = transform.position;
+        positionR2.x = positionR.x;
+        positionR2.y -= bc.bounds.extents.y;
+
+        if (isCollidingInDirection(positionR, Vector3.right) || isCollidingInDirection(positionR2, Vector3.right))
+        {
+            collidingDirection = wallCollidingDirection.Right;
+        }
+
+        Vector2 positionL = transform.position;
+        positionL.x -= bc.bounds.extents.x;
+        positionL.y += bc.bounds.extents.y;
+
+        Vector2 positionL2 = transform.position;
+        positionL2.x = positionL.x;
+        positionL2.y -= bc.bounds.extents.y;
+
+
+        if (isCollidingInDirection(positionL, Vector3.left) || isCollidingInDirection(positionL2, Vector3.left))
+        {
+            if (collidingDirection != wallCollidingDirection.None)
+            {
+                collidingDirection = wallCollidingDirection.Booth;
+            }
+            else
+            {
+                collidingDirection = wallCollidingDirection.Left;
+            }
+        }
+
+        return collidingDirection;
+    }
+
+    private bool isCollidingInDirection(Vector2 position, Vector3 direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, MARGIN_TO_COLLIDE, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    #endregion
+}
